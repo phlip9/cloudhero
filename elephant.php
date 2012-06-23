f110531b7aad38f48614d284594b3ffb61b1cafc
Philip Hayes <philiphayes9@gmail.com>
2012-06-23 12:25:23 -0700

    Woah, this is a commit; check out the cooool stuff

---
+++ elephant.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "php the elephant";
+echo "<img src='http://workbench.cadenhead.org/media/php-elephant-icon.jpg' />";
+
+?>
---
+++ monty.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# This is python
+
+print("Ham is good")
+print("Ham, Spam, Eggs")
